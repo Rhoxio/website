5e8f77f82bcfb1c28578aa010643ce42849e2d2d
Rhoxio <rhoxiodbc@gmail.com>
2015-02-08 21:17:07 -0800

    initial commit for website

---
+++ javascript.js
@@ -0,0 +1,21 @@
+ sayingArray = ['To change the world, you need only to speak your mind.', 'To live is to experience, and the art of learning is based upon it.', 'Inspiration is a product of passion, not a result of it.']
+
+$( document ).ready(function(){
+	$('.tagline').html(sayingArray[Math.floor(Math.random() * sayingArray.length)])
+	
+	var tagFadeIn = function(){
+		$('.tagline').fadeIn(2000, function(){
+			console.log("Working?")
+		})
+	}
+
+
+	var nameFadeIn = function(){
+		$('.name-desc').fadeIn(1000, function(){
+			tagFadeIn()
+		})
+	}
+
+	nameFadeIn()	
+})
+ 
